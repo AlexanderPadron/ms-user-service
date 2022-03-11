74dbddfdbba0992d4b2c16815f680a6990bb5908
Alexander Padron <alexander.padron@externos.bci.cl>
2022-03-10 21:21:59 -0300

    conection mongo

---
+++ src/main/java/cl/venture/userservice/entity/User.java
@@ -0,0 +1,19 @@
+package cl.venture.userservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "user")
+public class User {
+    @Id
+    private String _id;
+    private String name;
+    private String email;
+
+}
---
+++ src/main/java/cl/venture/userservice/service/UserService.java
@@ -0,0 +1,27 @@
+package cl.venture.userservice.service;
+
+import cl.venture.userservice.entity.User;
+import cl.venture.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User save(User user) {
+        User userNew = userRepository.save(user);
+        return userNew;
+    }
+}
